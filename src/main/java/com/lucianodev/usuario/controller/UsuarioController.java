ad4739cd7ba4395fd721d6e4fdf6ca59142cc15f
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-04-13 11:57:17 -0300

    adiciona metodos GET, DELETE LOGIN

--- src/main/java/com/lucianodev/usuario/controller/UsuarioController.java
+++ src/main/java/com/lucianodev/usuario/controller/UsuarioController.java
@@ -2,12 +2,14 @@ package com.lucianodev.usuario.controller;
 
 import com.lucianodev.usuario.business.UsuarioService;
 import com.lucianodev.usuario.business.dto.UsuarioDto;
+import com.lucianodev.usuario.infrastructure.entity.Usuario;
+import com.lucianodev.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -18,11 +20,33 @@ import java.net.URI;
 public class UsuarioController {
 
     private final UsuarioService service;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<UsuarioDto> save(@RequestBody UsuarioDto dto) {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(service.save(dto));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDto usuarioDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDto.getEmail(),
+                        usuarioDto.getSenha())
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> searchUserByEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(service.searchUserByEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> delete(@PathVariable String email) {
+        service.deleteByEmail(email);
+        return ResponseEntity.noContent().build();
     }
 }
